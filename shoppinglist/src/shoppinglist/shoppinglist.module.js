0750f7596782c4671fc2c2f2e18ad1e5ad3aedd2
gsipos_ccn <gsipos@ccn.sk>
2023-01-05 08:44:35 +0100

    config run logging

--- shoppinglist/src/shoppinglist/shoppinglist.module.js
+++ shoppinglist/src/shoppinglist/shoppinglist.module.js
@@ -3,4 +3,12 @@
 
 angular.module('ShoppingList',['Spinner']);
 
+angular.module('ShoppingList')
+.config(function () {
+    console.log("ShoppingList config fired.");
+})
+.run(function () {
+    console.log("ShoppingList run fired.");
+});
+
 })();
